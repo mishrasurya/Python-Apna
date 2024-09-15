683e17765d4a0bfa31bc7f20705f494165069280
mishrasurya <mishrasurya543@gmail.com>
2024-09-15 19:44:40 +0530

    tupple modified

--- ListTuppleExcercise.py
+++ ListTuppleExcercise.py
@@ -1,19 +1,19 @@
-# # WAP to ask the user to enter names of their 3 favorite movies & store them in a list.
-# # 1st approach
-# movies=[ ]
-# mov=input("enter first movie: ")
-# movies.append(mov)
-# mov=input("enter second movie: ")
-# movies.append(mov)
-# mov=input("enter third movie: ")
-# movies.append(mov)
-# print(movies) #['dlj', 'kobra', 'mohra']
-# # Second approach
-# movies=[ ]
-# movies.append(input("enter first movie: "))
-# movies.append(input("enter second movie: "))
-# movies.append(input("enter third movie: "))
-# print(movies)
+# WAP to ask the user to enter names of their 3 favorite movies & store them in a list.
+# 1st approach
+movies=[ ]
+mov=input("enter first movie: ")
+movies.append(mov)
+mov=input("enter second movie: ")
+movies.append(mov)
+mov=input("enter third movie: ")
+movies.append(mov)
+print(movies) #['dlj', 'kobra', 'mohra']
+# Second approach
+movies=[ ]
+movies.append(input("enter first movie: "))
+movies.append(input("enter second movie: "))
+movies.append(input("enter third movie: "))
+print(movies)
 
 #WAP to check if a list contains a palindrome of elements. (Hint: use copy( ) method)
 list=[1,2,3,2,1]
